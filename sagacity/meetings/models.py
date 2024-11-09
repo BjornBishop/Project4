411e382be52c081927319c3269bf8c3a6af72bb9
Bjorn Bishop <bjornbishop@gmail.com>
2024-11-09 22:18:20 +0000

    initial CSS done

--- sagacity/meetings/models.py
+++ sagacity/meetings/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-# meetings/models.py
+
 from django.db import models
 from django.core.exceptions import ValidationError
 from datetime import datetime, time
